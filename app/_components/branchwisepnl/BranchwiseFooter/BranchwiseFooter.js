e3c23e344ec05d52da7afe1a6310ba95af2f37dd
Marketlube <marketlubesoftwares@MarketlubesMini.Dlink>
2024-11-05 15:24:03 +0530

    configured redux importand commit

--- app/_components/branchwisepnl/BranchwiseFooter/BranchwiseFooter.js
+++ app/_components/branchwisepnl/BranchwiseFooter/BranchwiseFooter.js
@@ -1,19 +1,26 @@
 "use client";
 
+import { useSelector } from "react-redux";
 import BranchwiseFooterbtns from "./BranchwiseFooterbtns";
+import {
+  setBranchwiseBtnDisable,
+  setBranchwiseCurrentPage,
+} from "@/lib/slices/branchwiseSlice";
+import PageNavigate from "../../utils/_pagination/PageNavigate";
 
 function BranchwiseFooter() {
+  const { currentPage } = useSelector((state) => state.branchwise);
   return (
     <div className={`layout-footer`}>
       <div className="layout-footer-left">
         <BranchwiseFooterbtns />
       </div>
       <div className="layout-footer-right">
-        {/* <PageNavigate
-currentPage={currentPage}
-setCurrentPage={setDaybookCurrentPage}
-btnDisable={btnDisable}
-/> */}
+        <PageNavigate
+          currentPage={currentPage}
+          setCurrentPage={setBranchwiseCurrentPage}
+          btnDisable={setBranchwiseBtnDisable}
+        />
       </div>
     </div>
   );
