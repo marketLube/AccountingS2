68bad021eb656e33c7ed9c782eb95ffa9e87548c
marketLube <marketlubes@gmail.com>
2024-11-11 12:33:36 +0530

    Made balance sheet fixed

--- app/_components/_cards/_balance-card/TotalBalanceCard.js
+++ app/_components/_cards/_balance-card/TotalBalanceCard.js
@@ -5,18 +5,20 @@ function TotalBalanceCard() {
 
   return (
     <div className="balance-card">
-      <div className="balance-card-header">
-        <div className="small-text">Total Available balance</div>
-        <IoEyeSharp className="icons" />
-      </div>
+      <div>
+        <div className="balance-card-header">
+          <div className="small-text">Total Available balance</div>
+          <IoEyeSharp className="icons" />
+        </div>
 
-      <div className="balance-card-body">
-        {banks?.map((bank, i) => (
-          <div key={i} className="balance-card-item-box">
-            <span className="balance-card-name-box">{bank?.name}</span>
-            <span className="balance-card-balance"> {bank?.balance} $</span>
-          </div>
-        ))}
+        <div className="balance-card-body">
+          {banks?.map((bank, i) => (
+            <div key={i} className="balance-card-item-box">
+              <span className="balance-card-name-box">{bank?.name}</span>
+              <span className="balance-card-balance"> {bank?.balance} $</span>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="balance-card-balance-total">$ 755,88,97</div>
     </div>
