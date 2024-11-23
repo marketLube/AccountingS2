d06aa71c5f540dc297ee334413be3e45ab879d38
marketLube <marketlubes@gmail.com>
2024-11-23 13:16:51 +0530

    l

--- app/manage/page.js
+++ app/manage/page.js
@@ -3,11 +3,19 @@
 import ManageBank from "../_components/_manage/manageBank/ManageBank";
 import ManageBranch from "../_components/_manage/manageBranch/ManageBranch";
 import ManageProfileCard from "../_components/_manage/manageProfile/ManageProfileCard";
+import { useState } from "react";
 
 function page() {
+  // const [catagory, setCatagory] = useState("");
+  // const [particular, setParticular] = useState("");
   return (
     <div className={`layout manage`}>
       <h1 className={`main-head`}>Manage</h1>
+      {/* <Category
+        setCatagory={setCatagory}
+        setParticular={setParticular}
+        particular={particular}
+      /> */}
       <div className="manage-body">
         <ManageBank />
         <ManageBranch />
