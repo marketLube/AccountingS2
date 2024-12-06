02e63c913c456490ae62c7d843fa3ab1f56b3568
marketLube <marketlubes@gmail.com>
2024-12-06 18:27:24 +0530

    sl

---
+++ app/_components/_ledger/ledgerTable/LedgerParticular.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import LedgerParticularItems from "./LedgerParticularItems";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/lib/axiosInstance";
+
+function LedgerParticular() {
+  const { clickedParticular: item } = useSelector((state) => state.ledger);
+  const { data } = useQuery({
+    queryKey: ["particular-details"],
+    queryFn: () =>
+      apiClient
+        .get(`stats/particular-detail?particular=${item?.particularId}`)
+        .then((res) => res.data.result),
+  });
+
+  console.log(data, "data");
+
+  return (
+    <div className="ledger-particular">
+      <div className="ledger-particular-item-container">
+        {data?.map((item, i) => (
+          <LedgerParticularItems item={item} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LedgerParticular;
